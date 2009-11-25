2a87e7acc32b829d0d6667c030e17fa84f23c58b
Tom Dyer <tdyer1@gmail.com>
2009-11-25 11:11:09 -0500

    added cedet package and custom info dir

--- tdyer.el
+++ tdyer.el
@@ -25,4 +25,40 @@
 (eshell)
 (rename-buffer "xxx")
 
+;; add a local info directory for text-info files
+;; Ex: Installing 
+;; cd ~/.emacs.d/tdyer/cedet-1.0pre6
+;; sudo make install-info PREFIX=~/.emacs.d/tdyer
+(add-to-list 'Info-directory-list (concat dotfiles-dir "/tdyer/share/info"))
 
+
+;; Load CEDET.
+;; (add-to-list 'load-path (concat dotfiles-dir "tdyer/cedet-1.0pre6"))
+(load-file "~/.emacs.d/tdyer/cedet-1.0pre6/common/cedet.el")
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+
+;; Enabling Semantic (code-parsing, smart completion) features
+;; Select one of the following:
+
+;; * This enables the database and idle reparse engines
+;; (semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode
+;;   imenu support, and the semantic navigator
+;; (semantic-load-enable-code-helpers)
+
+;; * This enables even more coding tools such as intellisense mode
+;;   decoration mode, and stickyfunc mode (plus regular code helpers)
+(semantic-load-enable-gaudy-code-helpers)
+
+;; * This enables the use of Exuberent ctags if you have it installed.
+;;   If you use C++ templates or boost, you should NOT enable it.
+;; (semantic-load-enable-all-exuberent-ctags-support)
+
+;; Enable SRecode (Template management) minor-mode.
+;; (global-srecode-minor-mode 1)
+
+
+(regen-autoloads)
