2a6d5f7c9841bd0282c06f29a0fdb7bdc2ad81e3
Tom Dyer <tdyer@ourstage.com>
2013-12-27 14:29:09 -0500

    Added flyspell.

--- tdyer.el
+++ tdyer.el
@@ -317,5 +317,10 @@
         ))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
+;; Flyspell mode
+;; http://www-sop.inria.fr/members/Manuel.Serrano/flyspell/flyspell.html
+(autoload 'flyspell-mode "flyspell" "On-the-fly spelling checker." t)
+(autoload 'flyspell-delay-command "flyspell" "Delay on command." t) (autoload 'tex-mode-flyspell-verify "flyspell" "" t)
+
 ;; MUST BE THE LAST LINE IN FILE
 (regen-autoloads)
