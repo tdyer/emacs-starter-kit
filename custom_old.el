84d5cd94476f675454d43885e970972487914222
Tom Dyer <tdyer@ourstage.com>
2014-01-11 10:59:53 -0500

    Save old custom.el

---
+++ custom_old.el
@@ -0,0 +1,58 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets
+   (quote
+    (("apple" "monaco*" "medium" "r" "normal" 9 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 10 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 11 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 12 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 13 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 14 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 16 "monaco")
+     ("apple" "monaco*" "medium" "r" "normal" 18 "monaco")
+     ("apple" "lucida grande*" "medium" "r" "normal" 9 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 10 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 11 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 12 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 13 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 14 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 16 "lucida")
+     ("apple" "lucida grande*" "medium" "r" "normal" 18 "lucida")
+     ("apple" "lucida sans typewrite*" "medium" "r" "normal" 9 "lucida_typewriter")
+     ("apple" "lucida sans typewrite*" "medium" "r" "normal" 10 "lucida_typewriter")
+     ("apple" "lucida sans typewrite*" "medium" "r" "normal" 12 "lucida_typewriter")
+     ("apple" "lucida sans typewrite*" "medium" "r" "normal" 14 "lucida_typewriter")
+     ("apple" "lucida console*" "medium" "r" nil 11 "lucida_console")
+     (nil "courier*" "medium" "r" nil 11 "courier")
+     (nil "courier*" "medium" "r" nil 13 "courier")
+     (nil "bitstream vera sans mono" "medium" "r" "normal" 10 "vera_mono")
+     (nil "bitstream vera sans mono" "medium" "r" "normal" 12 "vera_mono")
+     (nil "bitstream vera sans mono" "medium" "r" "normal" 14 "vera_mono"))) t)
+ '(aquamacs-customization-version-id 300 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist
+    (quote
+     ((tool-bar-lines . 0)
+      (menu-bar-lines . 0)
+      (foreground-color . "Black")
+      (background-color . "White")
+      (cursor-type . box)
+      (cursor-color . "Red")
+      (internal-border-width . 0)
+      (left-fringe . 1)
+      (right-fringe)
+      (fringe)
+      (vertical-scroll-bars))))
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(one-buffer-one-frame-mode (quote default) nil (aquamacs-frame-setup))
+ '(visual-line-mode nil t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
