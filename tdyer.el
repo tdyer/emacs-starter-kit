0556c952f4df884beeeae08ecf38331ab84d4b8e
Tom Dyer <tdyer@ourstage.com>
2014-03-03 16:00:17 -0500

    turn off scroll, menu and toggle bars. make all emacs "~" backups go to .emacs.d/backups dir

--- tdyer.el
+++ tdyer.el
@@ -368,5 +368,23 @@
 (require 'expand-region)
 (global-set-key (kbd "C-=") 'er/expand-region)
 
+;; From http://whattheemacsd.com/
+;; Turn off mouse interface early in startup to avoid momentary
+;; display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 ;; MUST BE THE LAST LINE IN FILE
 (regen-autoloads)
