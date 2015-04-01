2b8ed71c3a5ccb23fab58cf5e714d205672a4a5a
Tom Dyer <tdyer1@gmail.com>
2015-04-01 17:24:13 -0400

    cask update and cash install

--- .cask/24.4.1/elpa/dash-20141220.1452/dash-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("dash.el") (21679 52797 845173 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
